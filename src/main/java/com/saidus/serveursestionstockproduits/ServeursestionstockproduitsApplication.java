a1e4068ae932ef74b1459659c7de9a2749ab6d46
saidMESMOUDI <saidmsz1@gmail.com>
2020-05-30 03:44:49 +0200

    first commit - App Server

---
+++ src/main/java/com/saidus/serveursestionstockproduits/ServeursestionstockproduitsApplication.java
@@ -0,0 +1,13 @@
+package com.saidus.serveursestionstockproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeursestionstockproduitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeursestionstockproduitsApplication.class, args);
+    }
+
+}
